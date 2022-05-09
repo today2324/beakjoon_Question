63f118bce2dfd5da5f0e9f8a48915be561a29fb9
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-09 16:59:30 +0900

    Update pizza_oven.cpp

--- pizza_oven.cpp
+++ pizza_oven.cpp
@@ -1,117 +1,75 @@
 #include<iostream>
 #include<vector>
-
-#define HOUR 60
-#define TEN 10
-#define MINUTE 1
-
 using namespace std;
 
+#define HOUR 60
+
 typedef struct Oventime {
-	int AddHour = 0;
-	int AddTen = 0;
-	int MinTen = 0;
-	int AddOne = 0;
-	int MinOne = 0;
+	int Addhour = 0;
+	int Addten = 0;
+	int Minten = 0;
+	int Addone = 0;
+	int Minone = 0;
 }Oventime;
 
 int main()
 {
-	Oventime ovenTime;
-	vector<int> setTimeN;
-	int TimeMinusN;
 	int testcase;
 	int temp;
+	int timeTemp;
 	cin >> testcase;
+	vector<int> setTime;
+	Oventime Time;
 
 	for (size_t i = 0; i < testcase; i++)
 	{
 		cin >> temp;
-		setTimeN.push_back(temp);
+		setTime.push_back(temp);
 	}
-
-	//for (size_t i = 0; i < testcase; i++)
-	//{
-	//	cout << setTimeN[i] << endl;
-	//
-	//}
-	//cout << endl;
 
 	for (size_t i = 0; i < testcase; i++)
 	{
-		if (HOUR < setTimeN[i])
+
+		while (60 < setTime[i])
 		{
-			for (size_t j = 0; HOUR <= setTimeN[i]; j++)
-			{
-				ovenTime.AddHour++;
-				setTimeN[i] -= HOUR;
-			}
+			Time.Addhour++;
+			setTime[i] -= HOUR;
 		}
 
-		if (35 < setTimeN[i])
-		{
-			ovenTime.AddHour++;
-			TimeMinusN = HOUR - setTimeN[i];
-
-			if (5 < TimeMinusN)
+		if (35 < setTime[i])
+		{		
+			Time.Addhour++;
+			timeTemp = setTime[i];
+			if (setTime[i] < 56)
 			{
-				ovenTime.MinTen++;
-				if (TimeMinusN >= TEN)
+				while (setTime[i] > 10)
 				{
-					TimeMinusN -= TEN;
+					setTime[i] -= 10;
+					Time.Addten++;
 				}
 			}
 
-			while (ovenTime.MinOne == TimeMinusN)
+			while (5 < (timeTemp % 10))
 			{
-				ovenTime.MinOne++;
-			}
-			setTimeN[i] = TimeMinusN;
-
-			if (!TimeMinusN)
-			{
-				setTimeN[i] = 0;
+				timeTemp++;
+				Time.Minone++;
 			}
 		}
-
-		if (5 < setTimeN[i] && setTimeN[i] < 36)
+		else
 		{
-			for (size_t j = 0; TEN < setTimeN[i]; j++)
-			{
-				ovenTime.AddTen++;
-				setTimeN[i] -= TEN;
-			}
 
-			if (5 < setTimeN[i] && setTimeN[i] <= TEN)
-			{
-				ovenTime.AddTen++;
-
-				while (ovenTime.MinOne != (TEN - setTimeN[i]))
-				{
-					ovenTime.MinOne++;
-				}
-				setTimeN[i] = 0;
-			}
 		}
 
-		while (setTimeN[i])
-		{
-			ovenTime.AddOne++;
-			setTimeN[i]--;
-		}
+		cout << Time.Addhour << " ";
+		cout << Time.Addten << " ";
+		cout << Time.Minten << " ";
+		cout << Time.Addone << " ";
+		cout << Time.Minone << endl;
 
-		cout << ovenTime.AddHour << " ";
-		cout << ovenTime.AddTen << " ";
-		cout << ovenTime.MinTen << " ";
-		cout << ovenTime.AddOne << " ";
-		cout << ovenTime.MinOne << endl;
-
-		ovenTime.AddHour = 0;
-		ovenTime.AddTen = 0;
-		ovenTime.MinTen = 0;
-		ovenTime.AddOne = 0;
-		ovenTime.MinOne = 0;
+		Time.Addhour = 0;
+		Time.Addten = 0;
+		Time.Minten = 0;
+		Time.Addone = 0;
+		Time.Minone = 0;
 	}
-
-
 }
