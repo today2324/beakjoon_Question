1e143286976cc81fcd3275ff41f01e5a5371619a
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-12 08:23:33 +0900

    Update toto.cpp

--- toto.cpp
+++ toto.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
-
 #define COUNTRYNUM 4
 using namespace std;
 
 typedef struct GroupStage {
-	string Ateam;
-	string Bteam;
-	double AWin;
+	string country;
+	double AWin[3] = { 0, };
 	double ALose;
 	double ADraw;
+	int addvancePercent;
 }GroupStage;
 
 GroupStage match[COUNTRYNUM];
@@ -23,32 +22,47 @@ int factorial(int n)
 	return n + factorial(n - 1);
 }
 
+void sub(string team, int AWin, int ADraw, int ALose)
+{
+	for (size_t j = 0; j < COUNTRYNUM; j++)
+	{
+		if (team == match[j].country)
+		{
+			if (!match[j].AWin)
+			{
+				match[j].AWin[j] = AWin;
+			}
+			match[j].ADraw = ADraw;
+			match[j].ALose = ALose;
+		}
+	}
+}
+
 int main()
 {
-	string country[4];
+	string Ateam;
+	string Bteam;
+	double AWin;
+	double ALose;
+	double ADraw;
 	for (size_t i = 0; i < COUNTRYNUM; i++)
 	{
-		cin >> country[i];
+		cin >> match[i].country[i];
 	}
 
-	for (size_t i = 0; i < factorial(COUNTRYNUM-1); i++)
+	for (size_t i = 0; i < factorial(COUNTRYNUM - 1); i++)
 	{
-		cin >> match[i].Ateam >> match[i].Bteam >> match[i].AWin >> match[i].ADraw >> match[i].ALose;
-		
-		for (size_t j = 0; j < COUNTRYNUM; j++)
-		{
-			if (match[j].Ateam == country[i])
-			{
-
-			}
-		}
-		
+		cin >> Ateam >> Bteam >> AWin >> ADraw >> ALose;
+	
+		sub(Ateam, AWin, ADraw, ALose);
+		sub(Bteam, AWin, ADraw, ALose);
 	}
 
+	
 
 	for (size_t i = 0; i < COUNTRYNUM; i++)
 	{
-		cout << endl;
+		cout << match[i].addvancePercent << endl;
 	}
 
 }
