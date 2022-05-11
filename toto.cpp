e5076c59f4179b6dee216e9929eca005980d631d
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-11 13:27:36 +0900

    Update toto.cpp

--- toto.cpp
+++ toto.cpp
@@ -3,37 +3,46 @@
 #define COUNTRYNUM 4
 using namespace std;
 
+typedef struct GroupStage {
+	string Ateam;
+	string Bteam;
+	double AWin;
+	double ALose;
+	double ADraw;
+}GroupStage;
+
+GroupStage match[COUNTRYNUM];
+
 int factorial(int n)
 {
 	if (n == 1)
 	{
 		return 1;
 	}
-	return n * factorial(n - 1);
+
+	return n + factorial(n - 1);
 }
 
 int main()
 {
-	int country[4];
-	int Ateam;
-	int Bteam;
-	double AWin;
-	double ALose;
-	double ADraw;
-
+	string country[4];
 	for (size_t i = 0; i < COUNTRYNUM; i++)
 	{
 		cin >> country[i];
 	}
 
-	for (size_t i = 0; i < factorial(COUNTRYNUM); i++)
+	for (size_t i = 0; i < factorial(COUNTRYNUM-1); i++)
 	{
-		cin >> Ateam >> Bteam >> AWin >> ADraw >> ALose;
-	
-		if ()
+		cin >> match[i].Ateam >> match[i].Bteam >> match[i].AWin >> match[i].ADraw >> match[i].ALose;
+		
+		for (size_t j = 0; j < COUNTRYNUM; j++)
 		{
-			
+			if (match[j].Ateam == country[i])
+			{
+
+			}
 		}
+		
 	}
 
 
