d6a5558aa5b426ded71835325c9d612d7ac045df
CENSORED <55275098+today2324@users.noreply.github.com>
2022-05-17 20:26:12 +0900

    Update virus.cpp

--- virus.cpp
+++ virus.cpp
@@ -5,55 +5,48 @@
 using namespace std;
 
 int computerNum;
-int computer[100];
+int computer[100][2];
 int network[100][2];
 bool visited[100] = { false, };
 int temp;
 int Count = 0;
 void virus_search(int i)
 {
-	if (computer[i] != INFECTION && visited[i] == false)
+	if (computer[i][0] != INFECTION && visited[i] == false)
 	{
-		computer[i] = INFECTION;
-		visited[i] = true;
+		computer[i][0] = INFECTION;
 		Count++;
-		for (int k = i; k < i; i++)
+		for (size_t j = 0; j < computerNum; j++)
 		{
-			if (network[i][0] == computer[i])
+			if (network[j][0] == computer[i][1])
 			{
 				virus_search(network[i][1]);
 			}
-		}
-		
-	}
-	else
-	{
-		if (network[i][0] == computer[i])
-		{
-			virus_search(network[i][1]);
+			if (network[j][1] == computer[i][1])
+			{
+				virus_search(network[j][0]);
+			}
 		}
 	}
+	visited[i] = true;
+	
 }
 
 int main()
 {
 	int temp;
 	int testcase;
-
 	cin >> computerNum;
 	cin >> testcase;
-
 	for (size_t i = 0; i < computerNum; i++)
 	{
-		computer[i] = i;
+		computer[i][1] = i + 1;
 	}
 
 	for (size_t i = 0; i < testcase; i++)
 	{
 		cin >> network[i][0] >> network[i][1];
 	}
-
-	virus_search(computer[0]);
-
+	virus_search(0);
 	cout << Count << endl;
 }
